8dbcbd5b7f47ec9e7e4f6c61422feb8b8e60479a
uvoz <uvoz@users.noreply.github.com>
2021-09-22 23:00:35 +0200

    Update hub.py

--- hub.py
+++ hub.py
@@ -3,7 +3,8 @@
 ## This is the source code to control the REFLEXE mirrorwall.
 
 ### Author                 : Bob Heida
-### Test and minor changes : MakerMatty (see commit history)
+### Tests and improvements : immakermatty Matěj Suchánek
+
 
 
 ### MIT License
